3b3f7a311b1fd9c9c513c6e4e4d394151333ac01
LorenzoOgnibene <lorenzo.ognibene@gmail.com>
2023-01-12 15:28:44 +0100

    Added component select todo (to fix, not working)

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,22 +1,29 @@
 <script>
-    import AppCard from './AppCard.vue';
     import axios from 'axios';
     import { store } from '../store'; 
     
+    import AppCard from './AppCard.vue';
+    import AppSelect from './AppSelect.vue';
+    
     export default{
         components:{
-            AppCard
+            AppCard,
+            AppSelect,  
         },
         data(){
             return{
                 store,
+                apiUrl : 'https://db.ygoprodeck.com/api/v7/cardinfo.php',
+                
             }
         },
         methods:{
             getCards(){
-                axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=10&offset=0',{
+                axios.get(this.apiUrl,{
                     params: {
-
+                        archetype : store.arcTypeVal,
+                        num : 10,
+                        offset : 0,
                     }
                 })
                 .then((response) => {
@@ -38,6 +45,7 @@
 
 <template>
     <div class="main-wrapper">
+        <AppSelect />
         <div class="container">
             <div class="row row-cols-lg-5 row-cols-md-3 row-cols-sm-2 d-flex">
                 <AppCard v-for="card in store.cardsList" :cardEl="card" />
---
+++ src/components/AppSelect.vue
@@ -0,0 +1,34 @@
+<script>
+import { store } from '../store';
+
+export default {
+    name: 'AppSelect',
+
+    data(){
+        return{
+            store, 
+            currentSel : ' ' ,
+            newVal : store.arcTypeVal,
+            
+        }
+    }
+}
+</script>
+
+
+<template>
+    <div class="select-wrapper">
+        <select v-model="currentSel" >
+            <option value="Alien">Alien</option>
+            <option value="Laval">Laval</option>
+            <option value="Vylon">Vylon</option>
+            <option value="Inzektor">Inzektor</option>
+            <option value="Umi">Umi</option>
+            <option value="Gusto">Gusto</option>
+        </select>
+    </div>
+</template>
+
+<style lang="scss">
+    
+</style>
